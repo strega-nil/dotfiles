02582a890981040da09d89edc1b4f85cd4fa03df
Nicole Patricia Mazzuca <nicole@strega-nil.co>
2025-03-28 19:15:22 +0100

    Allow bookmarks to be also the parent

--- fish/functions/fish_jj_prompt.fish
+++ fish/functions/fish_jj_prompt.fish
@@ -1,7 +1,27 @@
 function fish_jj_prompt
-  if ! jj status >/dev/null 2>&1
+  # make sure jj is installed
+  if ! command -sq jj
+    return 1
+  end
+  # check if we're in a jj repo
+  if ! jj root --quiet &>/dev/null
     return 1
   end
 
-  jj log --color=always -r "@" -T " ' (' ++ change_id.shortest() ++ if(bookmarks, '|' ++ bookmarks.join(' ')) ++ ')'" --no-graph
+  if test -n "$__fish_jj_prompt_bookmark_revset"
+    set prompt_bookmark_revset "$__fish_jj_prompt_bookmark_revset"
+  else
+    set prompt_bookmark_revset "@ | @-"
+  end
+
+  set current_head (jj log --color=always --no-graph \
+    -r "@" -T "change_id.shortest()")
+  set current_branch (jj log --color=always --no-graph \
+    -r "latest(($prompt_bookmark_revset) & bookmarks())" \
+    -T "bookmarks.join(' ')")
+  if test -n "$current_branch"
+    echo " ($current_head|$current_branch)"
+  else
+    echo " ($current_head)"
+  end
 end
